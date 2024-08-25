9b7df5129b932b22e82336b624dd38eeabcac7d9
pver0009 <pver0009@student.monash.edu>
2024-08-25 20:00:12 +1000

    created a database under init.py, and successfully done as a .db file has been created

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy #for the database
+from os import path #for the path of the database
 
 # Create a database object
 db = SQLAlchemy() #database object
@@ -24,6 +25,20 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    #check if we have created the database before the script is run
+    create_database(app)
 
     return app
 
+# Check if the database already exits if not then 
+# create it because otherwise if it does exist we don't want to override 
+# it because it contains data
+
+# Create the database
+def create_database(app):
+    #if the database does not exist create it
+    if not path.exists('website/' + DB_NAME): #check if the database exists
+        with app.app_context():
+            db.create_all() #create the database for the app and tell sqlalchemy which app to create the database for
+        print('Created Database!') 
