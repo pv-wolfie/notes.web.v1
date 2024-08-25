fcb702fa7e379dff5dd04bf5395d50595e3eae12
pver0009 <pver0009@student.monash.edu>
2024-08-25 21:49:04 +1000

    adding notes, home.html now shows a form to add notes, and views.py checks the note length, adds it to the database with the associated user id.

--- website/views.py
+++ website/views.py
@@ -1,12 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_user, login_required, logout_user, current_user
+from .models import Note
+from . import db
 
 #this file is a blueprint of our application, that includes urls to find the application
 
 views = Blueprint('views', __name__)
 
 # define a view, or a root in flask
-@views.route('/') #whenever we go onto our url and type in /, we will run the home/main page of the app
+@views.route('/', methods=['GET', 'POST']) #whenever we go onto our url and type in /, we will run the home/main page of the app
 @login_required # decorator to make sure that the user is logged in before they can access the home page
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Added!', category='success')
     return render_template("home.html", user = current_user) #reference current user to authenticate the user
